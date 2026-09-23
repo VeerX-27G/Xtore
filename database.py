9dba50b660a734d97ca7434dfa0c185f4efc43f7
VeerX-27G <gurveersinghx6@gmail.com>
2026-09-23 16:42:48 -0600

    fix in database.py

--- database.py
+++ database.py
@@ -8,8 +8,6 @@ if SQLALCHEMY_DATABASE_URL:
     # Heroku may provide either legacy postgres:// or standard postgresql:// URLs.
     if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
-    elif SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
-        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
     
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 else:
